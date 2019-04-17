e99d85a762a2ed098384bbc90d84c869c8c3bfb2
Jackson Henry <jackson.henry13@yahoo.com>
2019-04-17 13:48:41 -0500

    Initializing Firebase for the project.

---
+++ assets/trainSchedule.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCO93lv-KvOJAn8aKjTYZnIiV3deR3zFYM",
+    authDomain: "train-scheduler-d98e1.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d98e1.firebaseio.com",
+    projectId: "train-scheduler-d98e1",
+    storageBucket: "train-scheduler-d98e1.appspot.com",
+    messagingSenderId: "278872228356"
+};
+firebase.initializeApp(config);
+
+window.onload({
+    
+});
+
+
