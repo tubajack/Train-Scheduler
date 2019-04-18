391ec892668bd696751c8bf3b2fe65f18b1db9b1
Jackson Henry <jackson.henry13@yahoo.com>
2019-04-18 13:33:52 -0500

    Had to start over on the project due to a major error on my part. I deleted the old project on Firebase and created a new one. UPdated the initialize firebase section to accommodate for the new project.

--- assets/trainSchedule.js
+++ assets/trainSchedule.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCO93lv-KvOJAn8aKjTYZnIiV3deR3zFYM",
-    authDomain: "train-scheduler-d98e1.firebaseapp.com",
-    databaseURL: "https://train-scheduler-d98e1.firebaseio.com",
-    projectId: "train-scheduler-d98e1",
-    storageBucket: "train-scheduler-d98e1.appspot.com",
-    messagingSenderId: "278872228356"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyCObCno2oun2XkmjaBX__fIR0Ez03af7Yo",
+    authDomain: "train-scheduler-16447.firebaseapp.com",
+    databaseURL: "https://train-scheduler-16447.firebaseio.com",
+    projectId: "train-scheduler-16447",
+    storageBucket: "train-scheduler-16447.appspot.com",
+    messagingSenderId: "836220881869"
+  };
+  firebase.initializeApp(config);
 
 var database = firebase.database();
 
